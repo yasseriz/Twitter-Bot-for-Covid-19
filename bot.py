d7d06e81e64339e309bb0bc4448bc83725dfb1c3
yasseriz <yasserizaheer@gmail.com>
2020-05-01 23:46:44 +0800

    Final Update

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
+# Import Libraries
 import tweepy
 import sys
 import requests
 from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 from bs4 import BeautifulSoup
+from datetime import datetime
 
+# Authentication to Twitter API
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
+# Creating an API object
 api = tweepy.API(auth)
 
 try:
@@ -17,24 +21,39 @@ except:
     sys.exit(1)
 
 response = requests.get('https://www.worldometers.info/coronavirus/country/malaysia/')
-# print(response.content)
 soup = BeautifulSoup(response.content, 'lxml')
+
+# Obtaining daily data
+dateToday = datetime.today().strftime('%d %b %Y')
+daily = soup.find("li", {"class":"news_li"}).findAll("strong")
+source = soup.find("li", {"class":"news_li"}).find("a", {"class":"news_source_a"})
+
+cases = [detail.get_text() for detail in daily]
+
+# Obtaining total data
 numbers = soup.findAll("div", {"class": "maincounter-number"})
-# print(numbers)
 data = []
 dataName = ['Total Cases', 'Deaths', 'Recovered']
 for i in numbers:
-    data.append(i.text)
+    data.append(i.get_text().strip())
 
 newData = dict(zip(dataName, data))
 
+# Tweet
+tweet = f'''Coronavirus Latest Updates - Malaysia
 
-tweet = f'''Coronavirus Latest Updates
-Total cases: {newData['Total Cases']}
-Recovered: {newData['Recovered']}
-Deaths: {newData['Deaths']}
+Daily Cases - {dateToday}
+
+{cases[0]}
+{cases[1]}
+Source: {source["href"]}
+
+Total cases:{newData['Total Cases']} 
+Recovered:{newData['Recovered']} 
+Deaths:{newData['Deaths']} 
 Source: https://www.worldometers.info/coronavirus/
-#coronavirus #covid19 #coronavirusnews #coronavirusupdates
+
+#coronavirus #covid19 #coronavirusupdates
 '''
 
 api.update_status(tweet)
